9ba7637221da107a9a43dce0a20a34130e765c55
fwar34 <fwar34@126.com>
2017-12-30 18:41:10 +0800

    add org setting file for init.el

--- init.el
+++ init.el
@@ -4,19 +4,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; add feauther to load-path
-(add-to-list 'load-path "~/.emacs.d/lisp")
-
-(require 'init-minefunc)
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-org)
-(require 'init-better-default)
-(require 'init-keybindings)
-
-;; cl - Common List Extension
-(require 'cl)
-
-(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
-
-(load-file custom-file)
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "liang.org" user-emacs-directory))
