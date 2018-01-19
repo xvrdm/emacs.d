c47337541dfa4c80889f1a19582844d699ea262f
fwar34 <fwar34@126.com>
2018-01-19 20:32:55 +0800

    a

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -20,6 +20,12 @@
 
 ;; 更改字体大小
 ;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :height 160)
+(set-face-attribute 'default nil :height 120)
+
+;;设置窗口位置为屏库左上角(0,0)
+(set-frame-position (selected-frame) 200 80)
+;;设置宽和高
+(set-frame-width (selected-frame) 80)
+(set-frame-height (selected-frame) 30)
 
 (provide 'init-ui)
