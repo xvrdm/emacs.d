3b491709a27d6e454e14ed2b16d4b476862c3c22
fwar34 <fwar34@126.com>
2018-01-19 20:10:36 +0800

    a

--- init.el
+++ init.el
@@ -2,8 +2,32 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+;; (package-initialize)
+
+;; (require 'org-install)
+;; (require 'ob-tangle)
+;; (org-babel-load-file (expand-file-name "liang.org" user-emacs-directory))
+
+
+
 (package-initialize)
 
-(require 'org-install)
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "liang.org" user-emacs-directory))
+;; add feauther to load-path                                                                                      
+(add-to-list 'load-path "~/.emacs.d/lisp")                                                                        
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/lisp")                                                                                                                  
+(require 'init-minefunc)                                                                                          
+(require 'init-packages)                                                                                          
+(require 'init-ui)                                                                                                
+(require 'init-org)                                                                                               
+(require 'init-better-default)                                                                                    
+(require 'init-keybindings)                                                                                       
+(require 'init-evil)
+;; (require 'init-modeline)
+                                                                                                                  
+;; cl - Common List Extension                                                                                     
+(require 'cl)                                                                                                     
+                                                                                                                  
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))                                       
+                                                                                                                  
+(load-file custom-file)
+
