fa26ef7469d626887cb99a43235ae501880fd459
fwar34 <fwar34@126.com>
2019-03-28 15:28:22 +0800

    fix posframe

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -376,7 +376,7 @@
   ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
   ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
   ;; 手动安装 posframe 包。
-  (if (display-graphic-p)
+  (if (and (display-graphic-p) (>= emacs-major-version 26))
     (setq pyim-page-tooltip 'posframe)
    (setq pyim-page-tooltip 'popup))
 
