e402604858172a561f8932243ef281301dddbca2
fwar34 <fwar34@126.com>
2019-04-02 18:26:39 +0800

    remove spaceline-all-the-icons

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1019,16 +1019,16 @@
     (setq winum-auto-setup-mode-line nil)
     ;; (spaceline-spacemacs-theme))
     (spaceline-emacs-theme))
-  (use-package spaceline-all-the-icons
-    :after spaceline
-    :config
-    (spaceline-all-the-icons-theme)
-    (spaceline-all-the-icons--setup-anzu)            ;; Enable anzu searching
-    (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
-    (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
-    (spaceline-all-the-icons--setup-paradox)         ;; Enable Paradox mode line
-    (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
-    )
+  ;; (use-package spaceline-all-the-icons
+  ;;   :after spaceline
+  ;;   :config
+  ;;   (spaceline-all-the-icons-theme)
+  ;;   (spaceline-all-the-icons--setup-anzu)            ;; Enable anzu searching
+  ;;   (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
+  ;;   (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
+  ;;   (spaceline-all-the-icons--setup-paradox)         ;; Enable Paradox mode line
+  ;;   (spaceline-all-the-icons--setup-neotree)         ;; Enable Neotree mode line
+  ;;   )
   )
 
 (use-package winum
