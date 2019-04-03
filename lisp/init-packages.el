e9819e39827e099851ea7b11fa8fab64cc0c374c
fwar34 <fwar34@126.com>
2019-04-03 17:54:02 +0800

    add learn markdown

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1191,6 +1191,14 @@
   :delight symon-mode
   )
 
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown")
+  )
+
 ;; ace-window
 (use-package ace-window
   :delight
