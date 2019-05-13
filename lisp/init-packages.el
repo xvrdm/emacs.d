10b7c558a48379a7059b9c5b18fb2c491b2e1174
fwar34 <fwar34@126.com>
2019-05-13 19:04:31 +0800

    test ivy-rich for counsel-bookmark

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -202,6 +202,46 @@
   (setq ivy-format-function #'ivy-format-function-line)
   ;; To abbreviate paths using abbreviate-file-name (e.g. replace “/home/username” with “~”)
   (setq ivy-rich-path-style 'abbrev)
+  (setq ivy-rich-display-transformers-list
+        '(ivy-switch-buffer
+          (:columns
+           ((ivy-rich-candidate (:width 30))
+            (ivy-rich-switch-buffer-size (:width 7))
+            (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+            (ivy-rich-switch-buffer-project (:width 15 :face success))
+            (ivy-rich-switch-buffer-path (:width
+                                          (lambda (x)
+                                            (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+           :predicate
+           (lambda (cand) (get-buffer cand)))
+
+          counsel-M-x
+          (:columns
+           ((counsel-M-x-transformer (:width 40))
+            (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+
+          counsel-describe-function
+          (:columns
+           ((counsel-describe-function-transformer (:width 40))
+            (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+
+          counsel-describe-variable
+          (:columns
+           ((counsel-describe-variable-transformer (:width 40))
+            (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))
+
+          counsel-recentf
+          (:columns
+           ((ivy-rich-candidate (:width 0.8))
+            (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))
+
+          counsel-bookmark
+          (:columns
+           ((ivy-rich-bookmark-type)
+            (ivy-rich-candidate (:width 0.2))
+            (ivy-rich-bookmark-info (:face font-lock-comment-face))))
+          ))
   )
 
 ;; smartparens setting
