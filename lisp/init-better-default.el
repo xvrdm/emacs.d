f944b0181cb38ab5b96daf703e25affafc5b0ee7
fwar34 <fwar34@126.com>
2019-05-25 20:40:11 +0800

    fix coding in dired mode

--- lisp/init-better-default.el
+++ lisp/init-better-default.el
@@ -8,18 +8,18 @@
 ;; default-buffer-file-coding-system变量在emacs23.2之后已被废弃，使用buffer-file-coding-system代替
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
-(setq-default pathname-coding-system 'euc-cn)
-(setq file-name-coding-system 'euc-cn)
+;; (setq-default pathname-coding-system 'euc-cn)
+;; (setq file-name-coding-system 'euc-cn)
 ;; 另外建议按下面的先后顺序来设置中文编码识别方式。
 ;; 重要提示:写在最后一行的，实际上最优先使用; 最前面一行，反而放到最后才识别。
 ;; utf-16le-with-signature 相当于 Windows 下的 Unicode 编码，这里也可写成
 ;; utf-16 (utf-16 实际上还细分为 utf-16le, utf-16be, utf-16le-with-signature等多种)
-(prefer-coding-system 'cp950)
-(prefer-coding-system 'gb2312)
-(prefer-coding-system 'cp936)
+;; (prefer-coding-system 'cp950)
+;; (prefer-coding-system 'gb2312)
+;; (prefer-coding-system 'cp936)
 ;; (prefer-coding-system 'gb18030)
 ;; (prefer-coding-system 'utf-16le-with-signature)
-(prefer-coding-system 'utf-16)
+;; (prefer-coding-system 'utf-16)
 ;; 新建文件使用utf-8-unix方式
 ;; 如果不写下面两句，只写
 ;; (prefer-coding-system 'utf-8)
