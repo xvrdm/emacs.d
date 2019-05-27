765a8bd5a206cf4ad768e9d154f65ece94460298
fwar34 <fwar34@126.com>
2019-05-27 17:25:49 +0800

    adjust calendar

--- lisp/init-calendar.el
+++ lisp/init-calendar.el
@@ -3,8 +3,18 @@
 (use-package calendar
   :defer t
   :config
-  (message "XXXXXXXXXXXX")
+  (setq calendar-load-hook
+        '(lambda ()
+           (set-face-foreground 'diary-face "skyblue")
+           (set-face-background 'holiday-face "slate blue")
+           (set-face-foreground 'holiday-face "white")))
+  
   (setq calendar-view-holidays-initially-flag t)
+  ;; 设置阴历显示，在 calendar 上用 pC 显示阴历
+  (setq chinese-calendar-celestial-stem
+          ["甲" "乙" "丙" "丁" "戊" "己" "庚" "辛" "壬" "癸"])
+  (setq chinese-calendar-terrestrial-branch
+          ["子" "丑" "寅" "卯" "辰" "巳" "戊" "未" "申" "酉" "戌" "亥"])
   )
 
 (provide 'init-calendar)
