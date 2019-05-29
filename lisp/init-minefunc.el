25f5f8928608ce122b83511bfea9d535fb0d3676
fwar34 <fwar34@126.com>
2019-05-29 17:44:18 +0800

    fix make-thread in evil-delete

--- lisp/init-minefunc.el
+++ lisp/init-minefunc.el
@@ -314,7 +314,11 @@ URL `http://ergoemacs.org/emacs/elisp_run_current_file.html'"
                    (sleep-for 0 500)
                    (remove-overlays beg end 'face 'fwar34-hi-yellow))))
   )
-(advice-add #'evil-yank :after #'fwar34/highlight-yank)
+;; (advice-add #'evil-yank :after #'fwar34/highlight-yank) ;; delete also to use evil-yank
+(advice-add #'evil-yank-rectangle :after #'fwar34/highlight-yank)
+(advice-add #'evil-yank-lines :after #'fwar34/highlight-yank)
+(advice-add #'evil-yank-characters :after #'fwar34/highlight-yank)
+
 (advice-add #'lispyville-yank :after #'fwar34/highlight-yank)
 
 ;; 重新载入emacs配置
