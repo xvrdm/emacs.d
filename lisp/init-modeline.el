8261762dd61f92df666f4dc2f124f3e12dce6ef9
fwar34 <fwar34@126.com>
2019-05-29 17:29:31 +0800

    test for modeline to remove -- INSERT --

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -34,5 +34,55 @@
   ;; (spaceline-spacemacs-theme))
   (spaceline-emacs-theme))
 
+(use-package maple-modeline
+  :disabled
+  :load-path "lisp"
+  :hook (after-init . maple-modeline-init)
+  :config
+  ;; standard or minimal
+  (setq maple-modeline-style 'standard)
+  ;; standard or reset or some number
+  (setq maple-modeline-width 'standard))
+
+(use-package doom-modeline
+  :disabled
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+(use-package mood-line
+  :disabled
+  :ensure t
+  :hook
+  (after-init . mood-line-mode))
+
+(use-package smart-mode-line
+  :disabled
+  :ensure t
+  :init
+  (setq sml/theme 'light)
+  :hook
+  (after-init . (lambda () (sml/setup)))
+  )
+
+(use-package powerline
+  :disabled
+  :ensure t
+  :config
+  (powerline-default-theme)
+  )
+
+(use-package airline-themes
+  :disabled
+  :ensure t
+  :config
+  (load-theme 'airline-light)
+  )
+
+(use-package powerline-evil
+  :disabled
+  :ensure t
+  :config
+  ;; (setq powerline-evil-tag-style 'standard)
+  )
 
 (provide 'init-modeline)
