49546ae850d72b2714a49fe4c67190ab13687f9b
fwar34 <fwar34@126.com>
2020-03-18 11:03:14 +0800

    adjust

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -2,13 +2,13 @@
 
 ;; {{ auto-save.el
 ;; reference from https://github.com/redguardtoo/emacs.d
-(with-eval-after-load 'evil
+(eval-after-load 'evil
   (progn
     (local-require 'auto-save)
-    (add-to-list 'auto-save-exclude 'file-too-big-p t)
+    ;; (add-to-list 'auto-save-exclude 'file-too-big-p t)
     (setq auto-save-idle 2) ; 2 seconds
+    ;; (setq auto-save-slient t)
     (auto-save-enable)
-    (setq auto-save-slient t)
     ))
 ;; }}
 
